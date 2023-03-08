8e7acf1a5f32eae6c5db2df917e8102771af8da2
Mitnite <matvei.s2012@yandex.ru>
2023-03-08 17:45:20 +0300

    Add
    Added navigation menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import './App.css';
 import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
+import Drawer from "./components/Navigation/Drawer/Drawer";
 
 class App extends Component {
   render() {
@@ -15,50 +16,16 @@ class App extends Component {
 
     return (
         <BrowserRouter>
-          <div>
-            <nav className={'nav'}>
-              <ul>
-                <li>
-                  <NavLink
-                      to="/"
-                      style={({isActive}) =>
-                          isActive ? activeStyle : undefined
-                      }
-                  >
-                    Мой профиль
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                      to="/about"
-                      className={({isActive}) =>
-                          isActive ? activeClassName : undefined
-                      }
-                  >
-                    Врачи и клиники
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to={{pathname: '/cars'}}>
-                    Сообщения
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
+          <div className={"App"}>
 
-            <hr/>
+            <Drawer/>
 
             <Routes>
-              <Route path="/about" element={<h1>Мой профиль</h1>}/>
-              <Route path="/" element={<h1>Врачи и клиники</h1>}/>
-              <Route path="/cars" element={<h1>Сообщения</h1>}/>
+              <Route path="/" element={<h1>Мой профиль</h1>}/>
+              <Route path="/clinics" element={<h1>Врачи и клиники</h1>}/>
+              <Route path="/message" element={<h1>Сообщения</h1>}/>
             </Routes>
           </div>
-
-{/*          <div className="App">
-
-          </div>*/}
-
         </BrowserRouter>
 
     );
---
+++ src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,67 @@
+import React, {Component} from "react";
+import './Drawer.css'
+import {NavLink} from "react-router-dom";
+import Profile from '../../../img/navigation_img/Profile.svg'
+import Clinics from '../../../img/navigation_img/Clinics.svg'
+import Message from '../../../img/navigation_img/Message.svg'
+import Test from '../../../img/navigation_img/Test.svg'
+import About from '../../../img/navigation_img/About.svg'
+
+
+const links = [
+  {to: '/', label: 'Профиль', url: Profile},
+  {to: '/clinics', label: 'Врачи и клиники', url: Clinics},
+  {to: '/message', label: 'Сообщения', url: Message},
+  {to: '/tests', label: 'Тестирование', url: Test},
+  {to: '/about', label: 'Полезно знать', url: About},
+
+]
+
+class Drawer extends Component {
+
+  renderLinks() {
+    let activeClassName = "active";
+    return links.map((link, index) => {
+      return (
+          <NavLink
+              key={index}
+              to={link.to}
+              className={({isActive}) =>
+                  isActive ? activeClassName : undefined
+              }
+              style={{textDecoration: "none"}}
+          >
+            <li className={"link"}>
+
+              <img src={link.url} alt=""/>
+
+              <div style={{marginLeft: 15}}>
+                {link.label}
+              </div>
+
+            </li>
+          </NavLink>
+
+      )
+    })
+  }
+
+  render() {
+    return (
+        <React.Fragment>
+          <nav className={"Drawer"}>
+            <ul className={"Logo"}> Логотип</ul>
+            <ul>
+              {this.renderLinks()}
+            </ul>
+            <ul>
+              <button className={"drawer_button"}> Подать заявку </button>
+            </ul>
+          </nav>
+        </React.Fragment>
+    )
+  }
+
+}
+
+export default Drawer
